14e16082111c32d50c905e99c8581809791b90eb
sajanacharya404 <sesmaniraj7@gmail.com>
2024-06-06 20:54:29 +0545

    usenavigate used for navigate

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const About = () => {
-  return <div>About</div>;
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/");
+  };
+  return (
+    <div>
+      <button onClick={handleNavigate}>Login</button>
+    </div>
+  );
 };
 
 export default About;
