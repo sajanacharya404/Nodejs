c620aa586ffe29515538a64e01979c300277e6d3
sajanacharya404 <sesmaniraj7@gmail.com>
2024-06-13 20:53:13 +0545

    useeffect api call completed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,39 @@
-import React, { useEffect } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Loader from "../utils/Loader";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // we cant use async await inside useeffect
+  useEffect(() => {
+    getAllBooks();
+  }, []);
+
+  const getAllBooks = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get("http://localhost:8000/api/v1/allbooks");
+      const books = await res.data;
+      setBooks(books);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  if (loading) {
+    return <Loader />;
+  }
+  return (
+    <div>
+      {books.map((book) => (
+        <div key={book._id}>{book.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
